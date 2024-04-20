3e70f84799b689ac708490ff5177b7d4d7a02896
bleseneralexander <113368267+bleseneralexander@users.noreply.github.com>
2024-04-20 11:17:56 -0500

    Update sequel.sql

--- sequel.sql
+++ sequel.sql
@@ -8,3 +8,6 @@ column,
 end;
 create table with row = rows[1]
   create table with column = columns[3]
+where rows[0] = {column[0], column[0], column[0]}
+and row[1] = {column[1], column[1], column[1]}
+and row[i] = {column[i], column[i], column[i]}
