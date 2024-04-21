d7099419b49a9858d978e245ed1f5b741894eb46
bleseneralexander <113368267+bleseneralexander@users.noreply.github.com>
2024-04-21 04:45:00 -0500

    Update sequel.sql

--- sequel.sql
+++ sequel.sql
@@ -11,3 +11,4 @@ ALTER TABLE `donations` ADD CONSTRAINT `donations_card number_donations_card num
 ALTER TABLE `donations` ADD CONSTRAINT `donations_card exp_donations_card exp` FOREIGN KEY (`card exp`) REFERENCES `donations`(`card exp`);
 ALTER TABLE `donations` ADD CONSTRAINT `donations_card cvv_donations_card cvv` FOREIGN KEY (`card cvv`) REFERENCES `donations`(`card cvv`);
 ALTER TABLE `donations` ADD CONSTRAINT `donations_how much_donations_how much` FOREIGN KEY (`how much`) REFERENCES `donations`(`how much`);
+
